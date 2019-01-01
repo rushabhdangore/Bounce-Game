4b59ef78260f46e13ff464f52830c6a45862968d
Rushabh Dangore <rushabh.dangore@gmail.com>
2019-01-02 03:58:44 +0530

    App.js modified for path

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
 
 const app = express();
 const port = 8080;
 
-app.use('/',express.static(path.join('public/Bounce')));
+app.use('/',express.static(path.join(__dirname,'public/Bounce')));
 app.listen(port, () => { console.log("Port is listening") })
