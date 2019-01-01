862d1280d17bad526ba02c983dab30cb1e929a29
Rushabh Dangore <rushabh.dangore@gmail.com>
2019-01-02 02:57:44 +0530

    Home route set to static file of game

--- app.js
+++ app.js
@@ -5,10 +5,5 @@ const fs = require('fs');
 const app = express();
 const port = 8080;
 
-app.use('/bounce',express.static(path.join('public/Bounce'))) 
-app.get('/', (req,res) => {
-    res.writeHead(200, {'content-Type':'Text/html'});
-    res.write(fs.readFileSync('./sample.html'));
-    res.end();
-})
+app.use('/',express.static(path.join('public/Bounce'))) 
 app.listen(port, () => { console.log("Port is listening") })
